5c47588b1a33fe585770b9aaa38850170d6f522d
Alex_T <saler@inbox.ru>
2022-06-20 20:21:48 +0300

    Задача 03

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,28 @@
+package org.example;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        // Написать программу вычисления n -ного треугольного числа
+
+
+
+                System.out.println("Input member n : ");
+                Scanner scanner = new Scanner(System.in);
+                int x = scanner.nextInt();
+                scanner.close();
+
+                int a  = 0;
+                int sum = 0;
+                int count = 0;
+
+                while (count <= x) {
+                    a = a + 1;
+                    sum = sum + a;
+                    count = count + 1;
+                }
+
+                System.out.println("triangular number " + x + " level = " + sum);
+            }
+        }
+
---
+++ src/main/java/org/example/Task3.java
@@ -0,0 +1,24 @@
+// Написать программу вычисления n -ного треугольного числа
+
+import java.util.Scanner;
+public class Task3 {
+    public static void main(String[] args) {
+    
+        System.out.println("Введите число n : ");
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        scanner.close();
+      
+        int a  = 0;
+        int sum = 0;
+        int count = 0;
+
+        while (count <= x) {
+            a = a + 1;
+            sum = sum + a;
+            count = count + 1;
+        }
+        
+        System.out.println("Треугольное число " + x + " порядка = " + sum);
+   }
+}
